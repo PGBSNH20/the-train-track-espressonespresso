bce26ef94563e1225cb4b9ee9c7b549acb8ed3f8
Liam Björkman <liambjorkman00@outlook.com>
2021-03-08 14:08:21 +0100

    Interface init

---
+++ Source/TrainEngine/ITrackPlanner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine
+{
+    interface ITrackPlanner
+    {
+        ITrackPlanner FollowSchedule();
+        ITrackPlanner LevelCrossing();
+        ITrackPlanner CloseAt();
+        ITrackPlanner OpenAt();
+        ITrackPlanner SetSwitch();
+        ITrackPlanner StartTrainAt();
+        ITrackPlanner StopTrainAt();
+        ITrackPlanner ToPlan();
+    }
+}
---
+++ Source/TrainEngine/TrackPlanner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEngine
+{
+    class TrackPlanner : ITrackPlanner
+    {
+        public ITrackPlanner FollowSchedule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackPlanner LevelCrossing()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackPlanner CloseAt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackPlanner OpenAt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackPlanner SetSwitch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackPlanner StartTrainAt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackPlanner StopTrainAt()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackPlanner ToPlan()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
